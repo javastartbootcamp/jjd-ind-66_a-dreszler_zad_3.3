7b4042cf113ccfca101dff8013f71ae5b2855291
Aleksander Dreszler <olek.d93@gmail.com>
2023-02-10 21:07:06 +0100

    Stworzono klasę Calculator z metodami podanymi w zadaniu. Utworzono obiekt calc na podstawie klasy Calculator i wyświetlono na konsoli wyniki operacji na przykładowych liczbach.

---
+++ src/main/java/pl/javastart/task/Calculator.java
@@ -0,0 +1,18 @@
+package pl.javastart.task;
+public class Calculator {
+    boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    boolean isOdd(int number) {
+        return number % 2 != 0;
+    }
+
+    double circleField(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+    int power(int number) {
+        return number * number;
+    }
+}
